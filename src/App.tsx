6e20846691feeb3cee9c52110c9ed0101a8d8b79
Jakub Zygmunt <216938@edu.p.lodz.pl>
2021-08-31 11:44:56 +0200

    Configure eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <header className="App-header">Hello</header>
+        </div>
+    );
+};
 
 export default App;
