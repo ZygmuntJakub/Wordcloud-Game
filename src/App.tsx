f027e8779b3c71bce0ba659cee3a59691b0ed401
Jakub Zygmunt <216938@edu.p.lodz.pl>
2021-08-31 13:35:46 +0200

    Add react router with pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-const App = () => {
-    return (
-        <div className="App">
-            <header className="App-header">Hello</header>
-        </div>
-    );
-};
+import { BrowserRouter, Route } from "react-router-dom";
+import HomePage from "pages/HomePage";
+import QuestionPage from "pages/QuestionPage";
+import AnswerPage from "pages/AnswerPage";
+import ResultPage from "pages/ResultPage";
+
+const App = () => (
+    <BrowserRouter>
+        <Route exact path="/">
+            <HomePage />
+        </Route>
+        <Route path="/question">
+            <QuestionPage />
+        </Route>
+        <Route path="/answer">
+            <AnswerPage />
+        </Route>
+        <Route path="/result">
+            <ResultPage />
+        </Route>
+    </BrowserRouter>
+);
 
 export default App;
---
+++ src/pages/AnswerPage/AnswerPage.tsx
@@ -0,0 +1,5 @@
+const AnswerPage = () => {
+    return <div>Answer Page</div>;
+};
+
+export default AnswerPage;
